3307bcd575de3282a5eb4ac70f156138aaee95e2
Abdul Khan <akhan@lcs.on.ca>
2020-10-28 17:49:13 -0400

    Completed puzzle

--- WinningScore/main.swift
+++ WinningScore/main.swift
@@ -10,6 +10,55 @@
 
 import Foundation
 
+extension Int {
+    
+    static func collectInput(withPrompt prompt: String, minimum: Int?, maximum: Int?) -> Int {
+        
+        // Loop until a valid value is provided
+        while true {
+            
+            // Prompt the user
+            print(prompt)
+            
+            // Collect the input
+            guard let givenInput = readLine() else {
+                continue
+            }
+            
+            // Convert to an integer
+            guard let givenInteger = Int(givenInput) else {
+                continue
+            }
+            
+            // If a lowest value for the integer was specified...
+            if let minimumValue = minimum {
+                
+                // ... then check that the given integer is greater than or equal to the lowest desired value.
+                guard givenInteger >= minimumValue else {
+                    continue
+                }
+                
+            }
+            
+            // If an highest possible value for the integer was specified...
+            if let maximumValue = maximum {
+                
+                // ... then check that the given integer is less than or equal to the highest desired value.
+                guard givenInteger <= maximumValue else {
+                    continue
+                }
+                
+                
+            }
+            
+            // If we've made it past all the checks, the input is an integer in the desired range of values, so, return it
+            return givenInteger
+            
+        }
+    }
+}
+
+
 print("Winning Score")
 print("=============")
 
@@ -29,7 +78,7 @@ struct GameScore {
     
     // Computed property to determine points earned
     var totalPoints: Int {
-        return threePointShots * 3
+        return (threePointShots * 3) + (fieldGoals * 2) + (freeThrows)
     }
     
 }
@@ -39,11 +88,27 @@ var apples = GameScore()
 var bananas = GameScore()
 
 // INPUT
-apples.threePointShots = Int(readLine()!)!
-apples.fieldGoals = Int(readLine()!)!
+apples.threePointShots = Int.collectInput(withPrompt: "Three point shots in for Apples", minimum: 0, maximum: 100)
+apples.fieldGoals = Int.collectInput(withPrompt: "Field goals in for Apples", minimum: 0, maximum: 100)
+apples.freeThrows = Int.collectInput(withPrompt: "Free throws in for Apples", minimum: 0, maximum: 100)
+
+bananas.threePointShots = Int.collectInput(withPrompt: "Three point shots in for Bananas", minimum: 0, maximum: 100)
+bananas.fieldGoals = Int.collectInput(withPrompt: "Field goals for Bananas", minimum: 0, maximum: 100)
+bananas.freeThrows = Int.collectInput(withPrompt: "Free throw for Bananas", minimum: 0, maximum: 100)
 
 // PROCESS
-let pointSpreadAToB = apples.totalPoints - bananas.totalPoints
+print("")
 
+if apples.totalPoints > bananas.totalPoints {
+    let difference = apples.totalPoints - bananas.totalPoints
+    print("The Apples won by \(difference) points!")
+} else if apples.totalPoints < bananas.totalPoints {
+    let difference = bananas.totalPoints - apples.totalPoints
+    print("The Bananas won by \(difference) points!")
+} else {
+    print("Tie")
+}
+
+print("")
 // OUTPUT
 
